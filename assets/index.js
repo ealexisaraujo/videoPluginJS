807f498b2585d56822565b74e4ab7dd40b264735
Alexis Araujo <ala1289@gmail.com>
2019-09-04 12:00:32 -0500

    this

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,7 @@ import AutoPlay from './plugins/Autoplay.js'
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({ el: video, plugins: [
-  new AutoPlay()
+  //new AutoPlay()
 ] });
 
 const playButton = document.querySelector('#playButton')
